576dd92177ce42ef23dbcbcb7ba507c7d52af3ce
sakahyu <sakahyu.eng@gmail.com>
2022-08-30 22:29:00 +0900

    make config load

---
+++ go/todo_app/go.mod
@@ -0,0 +1,5 @@
+module github.com/sakahyu/output/go/todo_app
+
+go 1.18
+
+require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
---
+++ go/todo_app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sakahyu/output/go/todo_app/config"
+)
+
+func main() {
+	fmt.Println(config.Config)
+}
