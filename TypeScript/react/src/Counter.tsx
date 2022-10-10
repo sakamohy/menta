9c9291607dc5c1089245731f4ef405dc870119ba
HyumaSakamoto <hyuma_sakamoto@mediado.jp>
2022-10-10 22:18:27 +0900

    modified: React Hooks　追加。

--- TypeScript/react/src/Counter.tsx
+++ TypeScript/react/src/Counter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 // useState の戻り値の方は下記の通り。ジェネリクスに指定した型によって返ってくるメソッドが変わってくる。
 type MySetStateAction = number | ((prevState: number) => number);
@@ -7,13 +7,16 @@ type MySetStateAction = number | ((prevState: number) => number);
 const Counter: React.FC<{}> = () => {
   // しっかりと型定義をつけることによって型をしっかりと指定したメソッドや変数を取得することができる。
   const [count, setCount] = useState<number>(0);
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current++;
+  });
   return (
     <>
       <div>{count}</div>
-      <div>
-        <button onClick={() => setCount(count + 1)}>+1</button>
-        <button onClick={() => setCount(count - 1)}>-1</button>
-      </div>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
     </>
   );
 };
